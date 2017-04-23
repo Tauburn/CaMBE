cd466fdb582e1893192ad86bf27bd8a454b1cf1e
Tauburn <tauburn.da@gmail.com>
2017-04-23 15:02:24 +0100

    Commiting before I do some serious working.

--- src/base/battleSet/Battle.scala
+++ src/base/battleSet/Battle.scala
@@ -9,15 +9,22 @@ import base.actions.BasicActions
 //I'd like to use the Set for characters here so no duplicates but 'target' will be enemies therefore no Set
 case class Battle(character: PlayerCharacter, target: Monster) {
 
-  val turnOrder: List[Actor] = List(character, target)
-
   def battle(): Unit = {
-    //NB the | character is typed using ALT and 124
-    while (character.hitPoints >= 0 || target.hitPoints >= 0) {
-      turnOrder(1).asInstanceOf[Monster].copy(hitPoints = BasicActions.remainingHitPoints(turnOrder.head, turnOrder(1)))
-      turnOrder.head.asInstanceOf[PlayerCharacter].copy(hitPoints = BasicActions.remainingHitPoints(turnOrder(1), turnOrder.head))
+
+    def innerBattle(turnOrder: List[Actor]) {
       println(s"Character currently has ${turnOrder.head.hitPoints} hit points remaining")
       println(s"Monster currently has ${turnOrder(1).hitPoints} hit points remaining")
+
+      //NB the | character is typed using ALT and 124
+      if (turnOrder.head.hitPoints <= 0 || turnOrder(1).hitPoints <= 0) {
+        println("Winner decided!")
+      }
+      else {
+        val secondPlace = turnOrder(1).asInstanceOf[Monster].copy(hitPoints = BasicActions.remainingHitPoints(turnOrder.head, turnOrder(1)))
+        val firstPlace = turnOrder.head.asInstanceOf[PlayerCharacter].copy(hitPoints = BasicActions.remainingHitPoints(turnOrder(1), turnOrder.head))
+        innerBattle(List(firstPlace, secondPlace))
+      }
     }
+    innerBattle(List(character, target))
   }
 }
